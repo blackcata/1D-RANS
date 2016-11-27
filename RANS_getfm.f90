2857e08daabdab92441e3388d5d5478c3b727cc8
blackcata <blackcata109@gmail.com>
2016-11-27 19:27:05 +0900

    Modified the DO & IF statement order

--- RANS_getfm.f90
+++ RANS_getfm.f90
@@ -22,13 +22,13 @@
             ALLOCATE( Y_tmp(0:Ny), Rt(0:Ny))
             ALLOCATE( a(0:Ny), b(0:Ny), c(0:Ny), r(0:Ny), x(0:Ny) )
 
-            IF (mode ==4) THEN
+            IF (mode == 4) THEN
               !-----------------------------------------------------------!
               !                     Set TDMA constants
               !-----------------------------------------------------------!
               DO j = 1,Ny-1
                 Rt(j) = k(j)**2 /(nu * dis(j))
-                L     = Cp*sqrt(k(j)**3/dis(j)**2 + Ce**2*(nu**3/dis(j))**(1./4.))
+                L     = Cp*sqrt(k(j)**3/dis(j)**2 + Ce**2*(nu**3/dis(j))**(1./4))
                 C1    = Rt(j)**1.5/(A0*L)**2
 
                 a(j)  =   1
@@ -47,6 +47,7 @@
               c(0)  = 0
               r(0)  = 0
               r(Ny) = 0
+
               Rt(0) = 0
               Rt(Ny)= 0
 
@@ -63,28 +64,29 @@
                ELSE
                   Y_tmp(j) = 2*del - Y(j)
                END IF
+            END DO
 
-               SELECT CASE (mode)
-                  CASE(0) ! No wall model
-                    fm(j) = 1
+             SELECT CASE (mode)
+                CASE(0) ! No wall model
+                  fm(0:Ny) = 1
 
-                  CASE(1) ! van Driest (1954)
-                    fm(j) = ( 1 - exp(-Y_tmp(j)/(nu/u_tau)/A0))**2
+                CASE(1) ! van Driest (1954)
+                  fm(0:Ny) = ( 1 - exp(-Y_tmp(0:Ny)/(nu/u_tau)/A0))**2
 
-                  CASE(2) ! Launder and Sharma (1974)
-                    Rt(j) = k(j)**2 /(nu * dis(j))
-                    fm(j) = exp( -3.4/(1 + Rt(j)/50)**2 )
+                CASE(2) ! Launder and Sharma (1974)
+                  Rt(0:Ny) = k(0:Ny)**2 /(nu * dis(0:Ny))
+                  fm(0:Ny) = exp( -3.4/(1 + Rt(0:Ny)/50)**2 )
 
-                  CASE(4) ! Park et al (1997)
+                CASE(4) ! Park et al (1997)
+                  DO j = 0,Ny
                     fm1   = (1 + Cd*exp(-(Rt(j)/120)**2)*Rt(j)**(-3/4))*fw(j)**2
                     fm2   = 7.0*(4.5 + 0.3*prod(j)/dis(j))                      &
                                  /(4.5 + 1.3*prod(j)/dis(j))**2
 
                     fm(j) = fm1 * fm2
                     !print*,fm(j),fm1,fm2
-               END SELECT
-
-            END DO
+                  END DO
+             END SELECT
 
             DEALLOCATE(Y_tmp)
 
